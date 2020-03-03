ec4e6b1b19cf5ab482c709c8a39930c02339333d
Gustavo Martinez <gm3857@gmail.com>
2020-03-02 23:10:48 -0600

    Ajuste responsive

---
+++ js/clip.js
@@ -0,0 +1,11 @@
+/* var video = document.getElementById('clip');
+
+var video2 = 'https://download.blender.org/peach/trailer/trailer_400p.ogg';
+
+function cambiar(video) {
+    video.setAttribute('src', 'video2');
+    video.play();
+};
+
+cambiar();
+ */
